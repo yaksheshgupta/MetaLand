d2c85b4faae3b8f79221017c90551a5fcd6d7dca
Aman <krishanaman14@gmail.com>
2022-12-17 20:13:57 +0530

    landing

---
+++ web/src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+        <div className="Home_container">
+        <img className="Home_image" src="/img/MacBook Pro 16_ - 1.png" alt="landing" />
+        </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ web/src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    
+    <div className="Navbar">
+        {/* <img className="Logo" src="/" /> */}
+    <div className="Button">
+         <button> Buy your land</button>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar;
