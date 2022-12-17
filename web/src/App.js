6c1cdf20534316ce7cc92840d92ed5f05479d6ea
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2022-12-17 20:13:28 +0530

    c1

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import * as page from "./pages"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useAccount,WagmiConfig } from 'wagmi'
+import "./App.css";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient } from "wagmi";
+//   import {  configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet} from 'wagmi/chains'
 
+// const chains = [ chain.polygon, chain.optimism, chain.arbitrum];
+
+// Wagmi client
+const { chains, provider } = configureChains([mainnet], [
+  walletConnectProvider({ projectId: "0ec4a219df81a3fc7c09f6ea9dadfd24" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <WagmiConfig client={wagmiClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<page.Home/>}/>
+        </Routes>
+      </Router>
+    </WagmiConfig>
+      <Web3Modal
+        projectId="0ec4a219df81a3fc7c09f6ea9dadfd24"
+        ethereumClient={ethereumClient}
+      />
+    </>
   );
 }
 
 export default App;
+
---
+++ web/src/pages.js
@@ -0,0 +1,11 @@
+import {Content,Card} from "./component/content";
+import Navbar from "./component/navbar";
+export const Home = ()=>{
+    return(
+        <>
+        <Navbar/>
+        <Content/>
+        {/* <Card/> */}
+        </>
+    )
+}
